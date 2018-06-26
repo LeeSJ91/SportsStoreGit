d0aa64a5ce002d7a6e7cca1c38fddb23b247a5df
LeeSeoungJea <lsj903602@gmail.com>
2018-06-26 17:07:06 +0900

    test

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        public string Menu()
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
         {
-            return "Hello from NavConroller";
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<string> categories = repository.Product.Select(a => a.Category).Distinct().OrderBy(a=>a);
+
+            return PartialView(categories);
         }
 	}
 }
